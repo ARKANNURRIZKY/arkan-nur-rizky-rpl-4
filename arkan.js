09cbee0ff7a63c9a7516c38f4871196e1a9fd0be
arkan nur rizky <arkan_rizky_35rpl@student.smktelkom-mlg.sch.id>
2026-07-21 14:29:34 +0700

    Add files via upload
    data nama,asal nama sekolah,alasan ke smk telkom malang

---
+++ arkan.js
@@ -0,0 +1,6 @@
+
+console.log("arkan nur rizky bisa di pangil arkan");
+console.log("saya dari SMPN 1 WONOREJO");
+console.log("alasan saya ke smk telkom malang karena saya memiliki cita cita sebagai machine learning engineer");
+console.log("dan saya juga memiliki ketertarikan di bidan robotik seperti membuat drone");
+console.log("tetapi saya masih dalam pembelajaran semua jadi saya memilih smk telkom malang agar saya bisa mengembangkannya");
